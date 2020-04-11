801d4a814e452e185a6966393728b84eb783cd0f
Mohamed Abd El-Monem <43797096+MuhamedAhmed11@users.noreply.github.com>
2020-04-12 00:55:44 +0200

    Delete CollectionNavbar.test.js

--- CollectionNavbar.test.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import CollectionNavbar from "./CollectionNavbar";
-
-it("CollectionNavbar renders without crashing", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<CollectionNavbar></CollectionNavbar>, div);
-});
