f568ecc12d68c725b9c53235c15f0cfa8f5b5c76
Mohamed Abd El-Monem <43797096+MuhamedAhmed11@users.noreply.github.com>
2020-04-12 00:55:37 +0200

    Delete LikedTracksNavbar.test.js

--- LikedTracksNavbar.test.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import LikedTracksNavbar from "./LikedTracksNavbar";
-
-it("LikedTracksNavbar renders without crashing", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<LikedTracksNavbar></LikedTracksNavbar>, div);
-});
