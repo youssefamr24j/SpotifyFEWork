2a03eb1f7fdc0464a6669d77cb75c55b6f4ab249
Mohamed Abd El-Monem <43797096+MuhamedAhmed11@users.noreply.github.com>
2020-04-12 00:54:04 +0200

    Delete SearchNavbar.test.js

--- SearchNavbar.test.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import SearchNavbar from "./SearchNavbar";
-
-it("SearchNavbar renders without crashing", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<SearchNavbar></SearchNavbar>, div);
-});
