1a8b1d2926cb859110daeecf2fc670f9cde079ea
youssefamr24j <43791846+youssefamr24j@users.noreply.github.com>
2020-04-11 23:32:45 +0200

    Update YourHeavyRotation.js

--- src/Components/YourHeavyRotation.js
+++ src/Components/YourHeavyRotation.js
@@ -2,6 +2,11 @@ import React from 'react';
 import ArtistCard from './ArtistCard';
 
 
+/**
+ * it takes an array or object that has id ,name,photo and type to send them to Artistcard class which visulaize each one of them
+ * we use this function to create many cards and header for them
+ */
+
 const YourHeavyRotation = ({Artists}) =>{
     const cardComponents=Artists.map(user => {
         return <ArtistCard key={user.id}  name={user.name} photo={user.photo} type={user.type} />
