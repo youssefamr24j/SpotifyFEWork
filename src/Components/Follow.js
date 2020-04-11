22d66c2f4412df99f9f77a3a1545aedd3ec91b26
youssefamr24j <43791846+youssefamr24j@users.noreply.github.com>
2020-04-11 23:37:30 +0200

    Update Follow.js

--- src/Components/Follow.js
+++ src/Components/Follow.js
@@ -1,15 +1,21 @@
 import React  from 'react';
 import './Follow.css';
 
+/**
+ * follow button it reacts to user click
+ */
 
 class Follow extends React.Component {
     constructor(props) {
       super(props);
       this.state = {isToggleOn: true};
   
-      // This binding is necessary to make `this` work in the callback
+      /**  This binding is necessary to make `this` work in the callback */
       this.handleClick = this.handleClick.bind(this);
     }
+     /**
+     * toggle the boolean "isToggleOn"
+     */
   
     handleClick() {
       this.setState(prevState => ({
@@ -19,6 +25,9 @@ class Follow extends React.Component {
   
     render() {
       return (
+          /**
+         * it converts the button depending on isToggleOn value 
+         */
         <div onClick={this.handleClick}>
           {this.state.isToggleOn ? <li id="follow-btn"> Follow </li> : <li id="unfollow-btn"> UNFOLLOW </li>}
         </div>
