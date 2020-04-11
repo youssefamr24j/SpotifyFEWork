069e658085f8d1a43256f01ca32705527883cc36
mariiamamr <61082089+mariiamamr@users.noreply.github.com>
2020-04-11 16:53:25 +0200

    Add files via upload

--- src/Components/AcountSideBar.js
+++ src/Components/AcountSideBar.js
@@ -3,60 +3,62 @@ import  "./AcountSideBar.css";
 
 class AcountSideBar extends React.Component {
 render(){
-return(
-<div class="side">
-    
-    <nav id="sidebar">
-        <div class="sidebar-header">
-            <h3>Account Info</h3>
-        </div>
+    return(
+    <div class="side">
+        
+        <nav id="sidebar">
+            <div class="sidebar-header">
+                <h3>Account Info</h3>
+            </div>
 
-        <ul class="list-unstyled components">
+            <ul class="list-unstyled components">
 
 
-        <img
-              className="logo"
-              src="https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_CMYK_White.png"
-              alt="logo"
-              width="auto"
-              height="40"
-            />
-            
-            <li>
-                <a href="/AccountoverView" className="buttons">Account Overview</a>
-            </li>
-            <li>
-                <a href="/EditProfile"className="buttons">Edit Profile</a>
-            </li>
-            <li>
-                <a href="/Recover" className="buttons">Recover Playlist</a>
-            </li>
-            <li>
-                <a href="/Home" className="buttons">Change Password</a>
-            </li>
-            <li>
-                <a href="/Home" className="buttons">Privacy Setting</a>
-            </li>
-            <li>
-                <a href="/Home" className="buttons">Recipits</a>
-            </li>
-            <li>
-                <a href="/Home"className="buttons">Apps</a>
-            </li>
-            <li>
-                <a href="/Home"className="buttons">Notifications Setting</a>
-            </li>
-            <li>
-                <a href="/Home"className="buttons">Redeem</a>
-            </li>
-        </ul>
+            <img
+                className="logo"
+                src="https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_CMYK_White.png"
+                alt="logo"
+                width="auto"
+                height="40"
+                />
+                
+                <li>
+                    <a href="/AccountoverView" className="buttons">Account Overview</a>
+                </li>
+                <li>
+                    <a href="/EditProfile"className="buttons">Edit Profile</a>
+                </li>
+                <li>
+                    <a href="/Recover" className="buttons">Recover Playlist</a>
+                </li>
+                <li>
+                    <a href="/Home" className="buttons">Change Password</a>
+                </li>
+                <li>
+                    <a href="/Home" className="buttons">Privacy Setting</a>
+                </li>
+                <li>
+                    <a href="/Home" className="buttons">Recipits</a>
+                </li>
+                <li>
+                    <a href="/Home"className="buttons">Apps</a>
+                </li>
+                <li>
+                    <a href="/Home"className="buttons">Notifications Setting</a>
+                </li>
+                <li>
+                    <a href="/Home"className="buttons">Redeem</a>
+                </li>
+            </ul>
 
-    </nav>
-    
+        </nav>
+        
 
-    
-</div>
-);
+        
+    </div>
+    );
+    }
 }
-}
+
+
 export default AcountSideBar;
